6ab94e909eb3d31cf6eeb1320860c32bbb961d61
andrewevans0102 <andrewevans0102@gmail.com>
2019-08-05 21:12:49 -0400

    2 files modified, 3 added, 1 deleted

--- src/app/traditional/traditional.component.ts
+++ src/app/traditional/traditional.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-traditional',
   templateUrl: './traditional.component.html',
-  styleUrls: ['./traditional.component.css']
+  styleUrls: ['./traditional.component.scss']
 })
 export class TraditionalComponent implements OnInit {
 
