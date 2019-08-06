1ffa7940790b16b72f4a0703124074916719f93f
andrewevans0102 <andrewevans0102@gmail.com>
2019-08-05 21:11:32 -0400

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TraditionalComponent } from './traditional/traditional.component';
+import { ReactiveComponent } from './reactive/reactive.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, TraditionalComponent, ReactiveComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/traditional/traditional.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-traditional',
+  templateUrl: './traditional.component.html',
+  styleUrls: ['./traditional.component.css']
+})
+export class TraditionalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
