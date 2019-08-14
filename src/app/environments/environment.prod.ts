19b1d42d31043596eae7ee77c4343cea8ffc4631
Andrew Evans <andrewevans0102@gmail.com>
2019-08-14 06:39:57 -0400

    adding prod file settings

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  traditionalEndpoint: 'https://us-central1-learning-rxjs-with-angular.cloudfunctions.net/app/traditional',
+  reactiveEndpoint: 'https://us-central1-learning-rxjs-with-angular.cloudfunctions.net/app/reactive',
+  manualEntries: 'https://raw.githubusercontent.com/andrewevans0102/learning-rxjs-with-angular/master/src/app/posts.json'
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   traditionalEndpoint: 'https://us-central1-learning-rxjs-with-angular.cloudfunctions.net/app/traditional',
   reactiveEndpoint: 'https://us-central1-learning-rxjs-with-angular.cloudfunctions.net/app/reactive',
-  manualEntries: 'https://raw.githubusercontent.com/andrewevans0102/learning-rxjs/master/src/assets/posts.json'
+  manualEntries: 'https://raw.githubusercontent.com/andrewevans0102/learning-rxjs-with-angular/master/src/app/posts.json'
 };
 
 /*
