627514415f1aa826dc67e98ef30c978f79bb8e2e
Andrew Evans <andrewevans0102@gmail.com>
2019-08-14 06:38:52 -0400

    added firebase functions with deploy

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  traditionalEndpoint: 'https://us-central1-learning-rxjs-server-backend.cloudfunctions.net/app/traditional',
-  reactiveEndpoint: 'https://us-central1-learning-rxjs-server-backend.cloudfunctions.net/app/reactive',
+  traditionalEndpoint: 'https://us-central1-learning-rxjs-with-angular.cloudfunctions.net/app/traditional',
+  reactiveEndpoint: 'https://us-central1-learning-rxjs-with-angular.cloudfunctions.net/app/reactive',
   manualEntries: 'https://raw.githubusercontent.com/andrewevans0102/learning-rxjs/master/src/assets/posts.json'
 };
 
