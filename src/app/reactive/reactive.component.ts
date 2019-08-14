8a9ebaebfa92a2142175cd22c9ee53769a9c2ad5
Andrew Evans <andrewevans0102@gmail.com>
2019-08-14 06:23:53 -0400

    adding error handling and fixing issues with merge operator

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, merge, Subject } from 'rxjs';
-import { map, takeUntil } from 'rxjs/operators';
+import { map, takeUntil, catchError } from 'rxjs/operators';
 import { environment } from '../environments/environment';
 
 @Component({
@@ -12,52 +12,92 @@ import { environment } from '../environments/environment';
 export class ReactiveComponent {
 
   posts$: Observable<any>;
+  output = [];
   unsubscribe: Subject<void> = new Subject();
 
   constructor(public http: HttpClient) { }
 
-  async load() {
-    const medium = this.http.get(environment.reactiveEndpoint + '/medium');
-    const wordpress = this.http.get(environment.reactiveEndpoint + '/wordpress');
-    const devto = this.http.get(environment.reactiveEndpoint + '/devto');
-    const manualEntries = this.http.get(environment.manualEntries);
+  load() {
+    const medium = 
+      this.http.get(environment.reactiveEndpoint + '/medium')
+      .pipe(
+        catchError(err => {
+          throw 'error in source observable. Message: ' + err.message;
+        })
+      );
 
-    this.posts$ =
-      merge(medium, wordpress, devto, manualEntries)
-        .pipe(takeUntil(this.unsubscribe))
-        .pipe(map((output: []) => {
-          output.forEach((post: any) => {
-            const inputDate = new Date(post.pubDate);
-            post.pubDate = inputDate.toLocaleDateString('en-us') + ' at ' + inputDate.toLocaleTimeString('en-us');
+    const wordpress = 
+      this.http.get(environment.reactiveEndpoint + '/wordpress')
+      .pipe(
+        catchError(err => {
+          throw 'error in source observable. Message: ' + err.message;
+        })
+      );
 
-            if (post.sourceURL === 'https://blog.angularindepth.com/feed') {
-              post.sourceURL = 'Angular-In-Depth';
-            } else if (post.sourceURL === 'https://itnext.io/feed') {
-              post.sourceURL = 'ITNext';
-            } else if (post.sourceURL === 'https://medium.com/feed/@Andrew_Evans') {
-              post.sourceURL = 'Medium';
-            } else if (post.sourceURL === 'https://rhythmandbinary.com/feed') {
-              post.sourceURL = 'Rhythm and Binary';
-            } else if (post.sourceURL === 'https://dev.to/feed/andrewevans0102') {
-              post.sourceURL = 'DEV.TO';
-            }
-          });
+    const devto = 
+      this.http.get(environment.reactiveEndpoint + '/devto')
+      .pipe(
+        catchError(err => {
+          throw 'error in source observable. Message: ' + err.message;
+        })
+      );
 
-          output.sort((a: any, b: any) => {
-          // https://stackoverflow.com/questions/10123953/sort-javascript-object-array-by-date
-            const aDate = new Date(b.sortDate);
-            const bDate = new Date(a.sortDate);
-            return bDate.getMilliseconds() - aDate.getMilliseconds();
-          });
+    const manualEntries = 
+      this.http.get(environment.manualEntries)
+      .pipe(
+        catchError(err => {
+          throw 'error in source observable. Message: ' + err.message;
+        })
+      );
 
-          return output;
-        }));
+    try {
+      this.posts$ =
+        merge(medium, wordpress, devto, manualEntries)
+          .pipe(takeUntil(this.unsubscribe))
+          .pipe(map((response: []) => {
+            response.forEach((post: any) => {
+              const inputDate = new Date(post.pubDate);
+              post.pubDate = inputDate.toLocaleDateString('en-us') + ' at ' + inputDate.toLocaleTimeString('en-us');
+
+              if (post.sourceURL === 'https://blog.angularindepth.com/feed') {
+                post.sourceURL = 'Angular-In-Depth';
+              } else if (post.sourceURL === 'https://itnext.io/feed') {
+                post.sourceURL = 'ITNext';
+              } else if (post.sourceURL === 'https://medium.com/feed/@Andrew_Evans') {
+                post.sourceURL = 'Medium';
+              } else if (post.sourceURL === 'https://rhythmandbinary.com/feed') {
+                post.sourceURL = 'Rhythm and Binary';
+              } else if (post.sourceURL === 'https://dev.to/feed/andrewevans0102') {
+                post.sourceURL = 'DEV.TO';
+              }
+
+              this.output.push({
+                ...post, 
+                sort1: inputDate.getTime()
+              });
+            });
+
+            this.output.sort((a: any, b: any) => {
+              return b.sort1 - a.sort1;
+            });
+
+            return this.output;
+          }));
+    } catch (error) {
+      console.log('error');
+      console.log(error);
+    }
   }
 
   clear() {
     this.unsubscribe.next();
     this.unsubscribe.complete();
     this.posts$ = null;
+    this.output = [];
+  }
+
+  handleError(endpoint: string) {
+    return 'error occured when calling ' + endpoint;
   }
 
 }
